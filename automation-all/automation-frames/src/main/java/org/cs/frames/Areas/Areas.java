87480daf425668a6a748d6bd2acef232577c6208
cs294639693 <294639693@qq.com>
2016-12-07 18:14:37 +0800

    add

---
+++ automation-all/automation-frames/src/main/java/org/cs/frames/Areas/Areas.java
@@ -0,0 +1,13 @@
+package org.cs.frames.Areas;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+public class Areas extends WebElement {}
---
+++ automation-all/automation-project/src/main/java/org/cs/project/area/SearchArea.java
@@ -0,0 +1,12 @@
+package org.cs.project.area;
+
+import org.cs.frames.Areas.Areas;
+
+public class SearchArea extends Areas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
