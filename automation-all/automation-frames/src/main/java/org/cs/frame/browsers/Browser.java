001042479e7663ef649b420063e4a2291850f86e
cs294639693 <294639693@qq.com>
2016-12-07 14:47:58 +0800

    new add

---
+++ automation-all/automation-frames/src/main/java/org/cs/frame/browsers/Browser.java
@@ -0,0 +1,15 @@
+package org.cs.frame.browsers;
+
+public class Browser {
+	
+	public Browser(){
+		System.out.println( "Hello browser!" );
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ automation-all/automation-project/src/main/java/org/cs/project/automation_project/App.java
@@ -0,0 +1,13 @@
+package org.cs.project.automation_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
