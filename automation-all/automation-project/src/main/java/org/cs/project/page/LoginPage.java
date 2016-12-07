725736703097f27a8fb0ba83648fc4798111521a
cs294639693 <294639693@qq.com>
2016-12-07 17:29:27 +0800

    add

--- automation-all/automation-project/src/main/java/org/cs/project/page/LoginPage.java
+++ automation-all/automation-project/src/main/java/org/cs/project/page/LoginPage.java
@@ -1,10 +1,23 @@
 package org.cs.project.page;
 
+import org.cs.project.pages.Pages;
 
-public class LoginPage{
+
+public class LoginPage extends Pages{
+	
+	public LoginPage(String URL) {
+		super(URL);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String URL = null;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		LoginPage loginPage = new LoginPage("http://www.baidu.com");
 
 	}
 
